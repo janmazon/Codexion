8c982db5c31eeb42dadae99b9bc3846bfc80db9a
janmazon <jaaanire@gmail.com>
2026-09-08 10:58:50 +0200

    empezado el parseo en el main y actualizados los structs

--- coders/main.c
+++ coders/main.c
@@ -6,11 +6,44 @@
 /*   By: jcamarer <jcamarer@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/07 13:50:07 by jcamarer          #+#    #+#             */
-/*   Updated: 2026/09/07 13:51:23 by jcamarer         ###   ########.fr       */
+/*   Updated: 2026/09/07 16:56:45 by jcamarer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "structs.h"
 
-int	main(void)
-{}
+int	validate_number(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str[i])
+	{
+		printf("Error: argument must be an integer.\n");
+		return (1);
+	}
+	while (str[i])
+	{
+		if (i == 0 && str[i] == '-')
+		{
+			printf("Error: argument must be positive.\n");
+			return (1);
+		}
+		if (str[i] < '0' || str[i] > '9')
+		{
+			printf("Error: argument must be an integer.\n");
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 9)
+	{
+		printf("Error: invalid number of arguments. Expected 8.\n");
+		return (1);
+	}
+}
--- coders/structs.h
+++ coders/structs.h
@@ -6,7 +6,7 @@
 /*   By: jcamarer <jcamarer@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/07 13:14:38 by jcamarer          #+#    #+#             */
-/*   Updated: 2026/09/07 14:03:34 by jcamarer         ###   ########.fr       */
+/*   Updated: 2026/09/07 17:15:51 by jcamarer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,29 @@
 # define STRUCTS_H
 
 # include <pthread.h>
+# include <stdio.h>
 
 typedef enum    e_coder_status
 {
-    status_compiling,
-    status_debugging,
-    status_refactoring,
-    status_waiting,
-    status_burned,
+    STATUS_COMPILING,
+    STATUS_DEBUGGING,
+    STATUS_REFACTORING,
+    STATUS_WAITING,
+    STATUS_BURNED,
 }   t_coder_status;
 
 typedef enum    e_dongle_status
 {
-    status_available,
-    status_using,
-    status_cooldown,
+    STATUS_AVAILABLE,
+    STATUS_USING,
+    STATUS_COOLDOWN,
 }   t_dongle_status;
+
+typedef enum    e_scheduler
+{
+    FIFO,
+    EDF,
+}   t_scheduler;
 
 typedef struct  s_coder
 {
@@ -47,5 +54,17 @@ typedef struct  s_dongle
     long            last_release_time;
     pthread_mutex_t mutex;
 }   t_dongle;
+
+typedef struct  s_parameters
+{
+    int         number_of_coders;
+    long        time_to_burnout;
+    long        time_to_compile;
+    long        time_to_debug;
+    long        time_to_refactor;
+    int         number_of_compiles_required;
+    long        dongle_cooldown;
+    t_scheduler scheduler;
+}   t_parameters;
 
 #endif
