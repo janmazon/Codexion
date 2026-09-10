80d31b59d6e9b1a2ccba3db47b9b277c52ce15eb
janmazon <jaaanire@gmail.com>
2026-09-10 15:24:21 +0200

    main sin el comentario

--- coders/main.c
+++ coders/main.c
@@ -6,7 +6,7 @@
 /*   By: jcamarer <jcamarer@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/07 13:50:07 by jcamarer          #+#    #+#             */
-/*   Updated: 2026/09/10 14:00:31 by jcamarer         ###   ########.fr       */
+/*   Updated: 2026/09/10 15:24:02 by jcamarer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,38 +31,6 @@ static int	init_data(t_data *data, char **argv)
 	return (0);
 }
 
-/* static int start_simulation(t_data *data)
-{
-	int			i;
-	pthread_t	*threads;
-	pthread_t	monitor_thread;
-
-	threads = malloc(data->params.number_of_coders * sizeof(pthread_t));
-	if (!threads)
-		return (1);
-	i = 0;
-	while (i < data->params.number_of_coders)
-	{
-		pthread_create(&threads[i], NULL, coder_routine, &data->coders[i]);
-		i++;
-	}
-	
-	// Creamos el hilo del vigilante, pasándole t_data entero
-	pthread_create(&monitor_thread, NULL, monitor_routine, data);
-
-	// Esperamos al vigilante
-	pthread_join(monitor_thread, NULL);
-	
-	i = 0;
-	while (i < data->params.number_of_coders)
-	{
-		pthread_join(threads[i], NULL);
-		i++;
-	}
-	free(threads);
-	return (0);
-} */
-
 int	main(int argc, char **argv)
 {
 	t_data	data;
@@ -76,7 +44,5 @@ int	main(int argc, char **argv)
 		return (1);
 	if (init_data(&data, argv) == 1)
 		return (1);
-/* 	if (start_simulation(&data) == 1)
-		return (1); */
 	return (0);
 }
